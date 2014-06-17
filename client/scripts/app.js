b50bd39c0603faef64d854319fc0df72ebb51a81
neillobo <neillobo@outlook.com>
2014-06-16 20:54:36 -0700

    Done with Basic - jQuery makes all Spaghetti code

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,13 @@
 // YOUR CODE HERE:
 var app = {};
+app.rooms = {};
+app.friends = {};
+app.currentRoom = 'All Rooms';
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
 app.init = function() {};
 
-app.fetch =function(currentRoom) {
+app.fetch = function(currentRoom) {
     $.ajax({
       // always use this url
       url: app.server,
@@ -12,54 +15,61 @@ app.fetch =function(currentRoom) {
       data: { order: "-createdAt", limit: "15" },
       contentType: 'application/json',
       success: function (data) {
+        app.currentRoom = currentRoom;
         app.printMessages(data, currentRoom);
-
       },
       error: function (data) {
-
-        console.error('chatterbox: Failed to send message');
+        // console.error('chatterbox: Failed to send message');
       }
     });
   };
-app.rooms = {};
 
 app.printMessages = function(data, currentRoom) {
-console.log(currentRoom)
+
   currentRoom = currentRoom || false;
   app.clearMessages();
 
   for (var i = 0; i < data.results.length; i++) {
     var message = data.results[i];
     app.rooms[message.roomname] = true;
-    if(currentRoom === message.roomname){
-      var li = $('<li></li>').text(message.username + ': '+ message.text);
-      $('#chats').append(li);
-    } else if (currentRoom === false || currentRoom === 'All Rooms') {
-      var li = $('<li></li>').text(message.username + ': '+ message.text);
+    if(currentRoom === message.roomname || (currentRoom === false || currentRoom === 'All Rooms')){
+        var li = $('<li></li>').append('<a class="users" href="#" value="'+_.escape(message.username)+'">'+_.escape(message.username) + '</a>: '+ _.escape(message.text));
+       if(app.friends[_.escape(message.username)]){
+        console.log("Friends");
+        li = $('<strong></strong>').append(li);
+       } else {
+       }
       $('#chats').append(li);
     }
   }
 
+ $('.users').on('click',function(){
+    var friend = $(this)[0].innerHTML;
+    app.friends[friend] = true;
+    console.log(app.friends);
+    app.printFriends();
+  });
 
   $('#rooms').children().remove();
-   app.rooms['All Rooms'] = true;
+  app.rooms['All Rooms'] = true;
   for (var key in app.rooms) {
     var option = $('<option value="'+key+'"></option>').text(key);
     $('#rooms').append(option);
   }
   console.log(currentRoom);
 
-// debugger;
-  if(currentRoom){
-    $('#rooms').val(currentRoom);
-  } else {
-    $('#rooms').val('All Rooms');
+  (currentRoom) ? $('#rooms').val(currentRoom) : $('#rooms').val('All Rooms');
+};
+
+app.printFriends = function() {
+  $('#friends').children().remove();
+  for (var key in app.friends) {
+    var li = $('<li></li>').text(key);
+    $('#friends').append(li);
   }
 
 };
 
-// app.printRooms = function(rooms, currentRoom) {
-// };
 
 app.clearMessages = function() {
     $('#chats').children().remove();
@@ -86,8 +96,8 @@ app.send = function(message) {
 app.addMessage = function(message){
 
   app.send(message);
-
-  $('#chats')
-      .append('<li>'+message.username + ': '+message.text + '</li>');
+  app.fetch(app.currentRoom);
+  // $('#chats')
+  //     .prepend('<li>'+message.username + ': '+message.text + '</li>');
 };
 
