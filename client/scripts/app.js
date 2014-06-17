7a408f2648f4d24e243345dd4ec023f4381a5135
neillobo <neillobo@outlook.com>
2014-06-17 10:51:54 -0700

    Finsihed basic requirements with some spaghetti code, perfect for moving onto Backbone

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,10 +1,5 @@
 // YOUR CODE HERE:
 //
-//  implement better init
-//  document read in this file?
-//  create room button
-//  maybe have all rooms persistent, don't need to delete rooms that fall out
-//    i.e. have app.rooms and add 'All Rooms' at beginning
 //  refactor to pass tests
 //
 //
@@ -15,7 +10,58 @@ app.friends = {};
 app.currentRoom = 'All Rooms';
 app.server = 'https://api.parse.com/1/classes/chatterbox';
 
-app.init = function() {};
+app.init = function() {
+  var getQueryVariable = function(variable) {
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+           var pair = vars[i].split("=");
+           if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+  };
+
+  var username = getQueryVariable('username');
+  console.log(username);
+
+  $('#send').on('click',function(){
+    app.currentRoom = app.currentRoom || 'All Rooms';
+    console.log(app.currentRoom);
+    var message = {
+      'username' : username,
+      'text' : $('#input').val(),
+      'roomname' : app.currentRoom
+    };
+    console.log(app.currentRoom);
+    console.log(message);
+    app.addMessage(message);
+  });
+
+  $('#newRoom').on('click',function(){
+    var uncleanRoom = prompt('Which room do you want to make?');
+    var cleanRoom = _.escape(uncleanRoom);
+    addRoom(cleanRoom);
+  });
+
+  $('#rooms').on('change',function(){
+    $("select option:selected").each(function() {
+
+      app.currentRoom = $(this).text();
+      app.fetch($(this).text());
+
+    });
+  });
+  app.fetch()
+  setInterval(function(){
+    app.fetch(app.currentRoom);
+  },4000);
+};
+
+app.addRoom = function(cleanRoom) {
+  app.rooms[cleanRoom] = true;
+  app.currentRoom = cleanRoom;
+  app.fetch(app.currentRoom);
+}
 
 app.fetch = function(currentRoom) {
     $.ajax({
@@ -54,17 +100,17 @@ app.printMessages = function(data, currentRoom) {
  $('.users').on('click',function(){
     var friend = $(this)[0].innerHTML;
     app.friends[friend] = true;
-    console.log(app.friends);
+
     app.printFriends();
   });
 
+ // print rooms
   $('#rooms').children().remove();
   app.rooms['All Rooms'] = true;
   for (var key in app.rooms) {
     var option = $('<option value="'+key+'"></option>').text(key);
     $('#rooms').append(option);
   }
-  console.log(currentRoom);
 
   (currentRoom) ? $('#rooms').val(currentRoom) : $('#rooms').val('All Rooms');
 };
@@ -104,8 +150,13 @@ app.send = function(message) {
 app.addMessage = function(message){
 
   app.send(message);
+  var li = $('<li></li>').append('<a class="users" href="#" value="'+message.username+'">'+message.username + '</a>: '+ message.text);
+  if(app.friends[message.username]){
+    li = $('<strong></strong>').append(li);
+  }
+  $('#chats').prepend(li);
+
   app.fetch(app.currentRoom);
-  // $('#chats')
-  //     .prepend('<li>'+message.username + ': '+message.text + '</li>');
+
 };
 
